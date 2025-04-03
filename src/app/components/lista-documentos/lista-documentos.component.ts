48634b9825adf7b25d690ca074496c7d833f157a
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2025-04-03 15:30:52 -0300

    Verificação de existência de documentos(excluindo a ficha de identificação)

--- src/app/components/lista-documentos/lista-documentos.component.ts
+++ src/app/components/lista-documentos/lista-documentos.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { ModeloDocumento } from '../../modelos/documento.modelo';
 import { DocumentoComponent } from '../documento/documento.component';
 
@@ -8,6 +8,24 @@ import { DocumentoComponent } from '../documento/documento.component';
   templateUrl: './lista-documentos.component.html',
   styleUrl: './lista-documentos.component.css',
 })
-export class ListaDocumentosComponent {
+export class ListaDocumentosComponent implements OnInit {
   documentos = input.required<ModeloDocumento[]>();
+
+  ngOnInit(): void {
+    let existemDocumentos = true;
+    if (this.documentos().length === 0) {
+      existemDocumentos = false;
+    }
+    if (this.documentos().length > 0) {
+      const index = this.documentos().findIndex((documento) => {
+        documento.nome.toLocaleLowerCase() === 'ficha de identificação';
+      });
+      if (index !== -1 && this.documentos().length === 1) {
+        existemDocumentos = false;
+      }
+    }
+    if (!existemDocumentos) {
+      alert('Não há documentos juntados ao processo selecionado.');
+    }
+  }
 }
