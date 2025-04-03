75e4e9b07bc3fccf6c9c4f67f5dabedf21f56312
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2025-04-03 09:42:14 -0300

    Imlementação de buscar documentos

--- src/app/components/buscar-documentos/buscar-documentos.component.ts
+++ src/app/components/buscar-documentos/buscar-documentos.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-buscar-documentos',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './buscar-documentos.component.html',
-  styleUrl: './buscar-documentos.component.css'
+  styleUrl: './buscar-documentos.component.css',
 })
 export class BuscarDocumentosComponent {
-
+  private servicoForm = inject(FormBuilder);
+  formBuscar = this.servicoForm.group({
+    textoBusca: ['', [Validators.required]],
+  });
+  numeroProcessoNaoFormatado = signal<string | undefined>('');
+  aoMudarNumeroProcesso(evento: Event) {
+    const elemento = evento.target as HTMLInputElement;
+    const valor = elemento.value;
+    if (elemento.validity.valid && valor.length > 0) {
+      this.numeroProcessoNaoFormatado.set(
+        this.formBuscar.value.textoBusca?.replace(/\D/g, '')
+      );
+      console.log(this.numeroProcessoNaoFormatado());
+    }
+    console.log(this.formBuscar.value.textoBusca);
+  }
 }
